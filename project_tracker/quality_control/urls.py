fec6d4ab6a2be97b2e45610fae9780e978d3ef38
Vlad Oreshko <vlador03@gmail.com>
2024-05-06 13:31:22 +0300

    VIEW functions and classes homework

--- project_tracker/quality_control/urls.py
+++ project_tracker/quality_control/urls.py
@@ -4,9 +4,10 @@ from quality_control import views
 app_name = 'quality_control'
 
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
+    path('', views.IndexView.as_view(), name='index'),
     path('bugs/', views.bugs_list, name='bugs_list'),
-    path('bugs/<int:id>/', views.bug_id_detail, name='bug_id_detail'),
+    path('bugs/<int:bug_report_id>/', views.BugReportDetailView.as_view(), name='bug_id_detail'),
     path('features/', views.features_list, name='features_list'),
-    path('features/<int:id>', views.feature_id_detail, name='feature_id_detail'),
+    path('features/<int:feature_request_id>/', views.FeatureRequestDetailView.as_view(), name='feature_id_detail'),
 ]
--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
+from .models import BugReport, FeatureRequest
+from django.views import View
+from django.views.generic import ListView, DetailView
 
 
 def index(request):
@@ -13,16 +16,62 @@ def index(request):
 
 
 def bugs_list(request):
-    return HttpResponse("Cписок отчетов об ошибках")
-
-
-def bug_id_detail(request, id):
-    return HttpResponse(f"Детали бага {id}")
+    bug_reports = BugReport.objects.all()
+    bug_reports_html = '<h1>Список баг репортов</h1><ul>'
+    for bug_report in bug_reports:
+        bug_reports_html += (f"<li><a href='{bug_report.id}/'>{bug_report.title}</a>"
+                             f" - {bug_report.get_status_display()}</li>")
+    bug_reports_html += f'</ul>'
+    return HttpResponse(bug_reports_html)
 
 
 def features_list(request):
-    return HttpResponse("Список запросов на улучшение")
+    feature_requests = FeatureRequest.objects.all()
+    feature_request_html = '<h1>Список запросов на улучшение</h1><ul>'
+    for feature_request in feature_requests:
+        feature_request_html += (f"<li><a href='{feature_request.id}/'>{feature_request.title}</a> "
+                                 f" - {feature_request.get_status_display()}</li>")
+    feature_request_html += f"</ul>"
+    return HttpResponse(feature_request_html)
 
 
-def feature_id_detail(request, id):
-    return HttpResponse(f"Детали улучшения {id}")
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        bugs_list_url = reverse('quality_control:bugs_list')
+        features_list_url = reverse('quality_control:features_list')
+        html = (f"<h1>Система контроля качества</h1>"
+                f"<p><a href='{bugs_list_url}'>Список всех багов</a></p>"
+                f"<p><a href = '{features_list_url}'>Запросы на улучшение</a></p>")
+        return HttpResponse(html)
+
+
+class BugReportDetailView(DetailView):
+    model = BugReport
+    pk_url_kwarg = 'bug_report_id'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        bug_report = self.object
+        response_html = (f"<h1>{bug_report.title}</h1>"
+                         f"<p><strong>Описание:</strong> {bug_report.description}</p>"
+                         f"<p><strong>Статус:</strong> {bug_report.get_status_display()}</p>"
+                         f"<p><strong>Приоритет:</strong> {bug_report.priority}</p>"
+                         f"<p><strong>Проект:</strong> {bug_report.project.name}</p>"
+                         f"<p><strong>Задача:</strong> {bug_report.task.name}</p>")
+        return HttpResponse(response_html)
+
+
+class FeatureRequestDetailView(DetailView):
+    model = FeatureRequest
+    pk_url_kwarg = 'feature_request_id'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        feature_request = self.object
+        response_html = (f"<h1>{feature_request.title}</h1>"
+                         f"<p><strong>Описание:</strong> {feature_request.description}</p>"
+                         f"<p><strong>Статус:</strong> {feature_request.get_status_display()}</p>"
+                         f"<p><strong>Приоритет:</strong> {feature_request.priority}</p>"
+                         f"<p><strong>Проект:</strong> {feature_request.project.name}</p>"
+                         f"<p><strong>Задача:</strong> {feature_request.task.name}</p>")
+        return HttpResponse(response_html)
