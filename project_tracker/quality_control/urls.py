20b58e771696ce34ed8dc444ae6afebde9ad5729
Vlad Oreshko <vlador03@gmail.com>
2024-05-12 10:12:03 +0300

    Working with forms homework

--- project_tracker/quality_control/urls.py
+++ project_tracker/quality_control/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('bugs/<int:bug_report_id>/', views.BugReportDetailView.as_view(), name='bug_id_detail'),
     path('features/', views.features_list, name='features_list'),
     path('features/<int:feature_request_id>/', views.FeatureRequestDetailView.as_view(), name='feature_id_detail'),
+    path('bugs/new/', views.create_bug_report, name='create_bug_report'),
+    path('ajax/get-tasks-for-project/<int:project_id>/', views.get_tasks_for_project, name='get_tasks_for_project'),
+    path('features/new/', views.create_feature_request, name='create_feature_request'),
 ]
--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from django.urls import reverse
 from .models import BugReport, FeatureRequest
+from tasks.models import Project, Task
 from django.views import View
 from django.views.generic import ListView, DetailView
+from .forms import BugReportForm, FeatureRequestForm
 
 
 def index(request):
@@ -18,6 +20,33 @@ def bugs_list(request):
 def features_list(request):
     feature_requests = FeatureRequest.objects.all()
     return render(request, 'quality_control/features_list.html', {'feature_requests': feature_requests})
+
+
+def create_bug_report(request, project_id=None):
+    if request.method == 'POST':
+        form = BugReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('quality_control:bugs_list')
+    else:
+        form = BugReportForm()
+    return render(request, 'quality_control/bug_report_form.html', {'form': form})
+
+
+def create_feature_request(request):
+    if request.method == 'POST':
+        form = FeatureRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('quality_control:features_list')
+    else:
+        form = FeatureRequestForm()
+    return render(request, 'quality_control/feature_request_form.html', {'form': form})
+
+
+def get_tasks_for_project(request, project_id):
+    tasks = list(Task.objects.filter(project_id=project_id).values('id', 'name'))
+    return JsonResponse(tasks, safe=False)
 
 
 class IndexView(View):
